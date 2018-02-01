3c4108fa81ab21a8515602260e01befadb905eab
EddyAnn <2634817136@qq.com>
2018-02-02 01:41:10 +0800

    add fractal package

--- readme.md
+++ readme.md
@@ -2,14 +2,17 @@
 
 ```
     1. git clone git@github.com:EddyAnn/shop.git
-    2. yarn install Or npm install
-    3. cp .env.example .env
+    2. composer install
+    3. yarn install Or npm install
+    4. cp .env.example .env
+    5. php artisan key:generate
 
 ```
-
 
 > use packages
 ```
     1. jwt-auth:
     2. dingo-api:
 ```
+
+
--- routes/api.php
+++ routes/api.php
@@ -16,11 +16,11 @@ $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function ($api) {
-    $api->group([
-        'middleware' => 'api.throttle',
-        'limit' => config('api.rate_limits.sign.limit'),
-        'expires' => config('api.rate_limits.sign.expires'),
-    ], function ($api) {
+    // 游客可以访问的接口
+
+    // 需要 token 验证的接口
+    $api->group(['middleware' => 'api.auth,api.throttle'], function ($api) {
+        // 当前登录用户信息
         $api->get('version', function () {
             return response('this is version v1');
         });
